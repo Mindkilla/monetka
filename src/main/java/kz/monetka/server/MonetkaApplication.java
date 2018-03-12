c7cb85711ed01d4bf490538cde8075a207d7542c
SmiAnA <aa.smirnov@bssys.com>
2018-03-12 21:10:56 +0600

    cors

--- src/main/java/kz/monetka/server/MonetkaApplication.java
+++ src/main/java/kz/monetka/server/MonetkaApplication.java
@@ -3,7 +3,11 @@ package kz.monetka.server;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +15,16 @@ public class MonetkaApplication {
 
 	public static void main(String[] args) {
         new SpringApplicationBuilder(MonetkaApplication.class).bannerMode(Banner.Mode.OFF).run(args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
 	}
 }
